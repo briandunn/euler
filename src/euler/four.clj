85cb65e3ef6fbd21286f6a10868da4b32b4b2808
Brian Dunn <dev+briandunn@hashrocket.com>
2012-12-09 16:01:23 -0600

    solve 4

--- src/euler/four.clj
+++ src/euler/four.clj
@@ -3,31 +3,59 @@
 
 (ns euler.four (:use clojure.test))
 
+(defn digits-to-number
+  [v] (Integer. (clojure.string/join v)))
+
 (defn largest-n-digit-number
-  [n] (Integer. (reduce str (map (fn [_] "9") (range n)))))
+  [n] (digits-to-number (map (fn [_] "9") (range n))))
+
+(defn smallest-n-digit-number
+  [n] (digits-to-number (cons "1" (map (fn [_] "0") (range (- n 1))))))
 
 (defn largest-product-of-n-digit-numbers
   [n] (#(* % %) (largest-n-digit-number n)))
 
+(defn all-n-digit-numbers
+  [n]
+  (range (smallest-n-digit-number n) (+ 1 (largest-n-digit-number n))))
+
+(defn products-of-n-digit-numbers
+  [n]
+  (flatten
+    (let [v (all-n-digit-numbers n)]
+      (map (fn [j] (map (fn [i] (* i j)) v)) v))))
+
 (defn digits-of
-  [n] (rest (clojure.string/split (str 99999) #"")))
+  [n] (map #(Integer. %) (rest (clojure.string/split (str n) #""))))
 
 (defn palindromic?
-  [a]
+  [v]
   (or
-    (empty? a)
+    (empty? v)
     (and
-      (= (first a) (last a))
-      (palindromic? (vec (rest (pop a)))))))
+      (= (first v) (last v))
+      (palindromic? (vec (rest (pop v)))))))
 
+(defn palindromic-products-of-n-digit-numbers
+  [n]
+  (map digits-to-number
+      (filter palindromic?
+        (map
+          #(vec (digits-of %))
+          (products-of-n-digit-numbers n)))))
 
-(deftest test-largest-product-of-n-digit-numbers
+(deftest test-largest-n-digit-number
   (is (= 9 (largest-n-digit-number 1)))
-  (is (= 81 (largest-product-of-n-digit-numbers 1)))
-  (is (= (* 99 99) (largest-product-of-n-digit-numbers 2))))
+  (is (= 999 (largest-n-digit-number 3)))
+  )
+
+(deftest test-smallest-n-digit-number
+  (is (= 1 (smallest-n-digit-number 1)))
+  (is (= 100 (smallest-n-digit-number 3)))
+  )
 
 (deftest test-products-of-n-digit-numbers
-         )
+  (is (= '(1 2 3 4 5 6 7 8 9) (take 9 (products-of-n-digit-numbers 1)))))
 
 (deftest test-palindromic?
   (is (not (palindromic? [1 0])))
@@ -36,3 +64,10 @@
   (is (palindromic? [1 3 3 1]))
   (is (palindromic? [1 3 9 3 1]))
   (is (palindromic? [0])))
+
+(deftest test-largest-palindromic-product-of-n-digit-numbers
+  (is (= 9009 (last (sort (palindromic-products-of-n-digit-numbers 2))))))
+
+(defn -main []
+  (println (last (sort (palindromic-products-of-n-digit-numbers 3)))))
+
